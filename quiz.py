baeac19a9af6dff292095c7e238e2bb6fc1d2d76
Marianasofid <marianadelgado@colegioguanenta.edu.co>
2025-02-13 16:00:03 -0500

    actualizacion

--- quiz.py
+++ quiz.py
@@ -6,7 +6,18 @@ print("----- VERIFICAR TRIANGULO ----")
 print("------------------------------")
 
 # input
+a = int(input("Ingrese el valor correspondiente al primer lado de la figura: "))
+b = int(input("Ingrese el valor correspondiente al segundo lado de la figura "))
+c = int(input("Ingrse el valor correspondiente al tercer lado de la figura "))
 
 # processing
 
+if a + b > c and a + c > b and b + c > a:
+    r ='Si se puedee formar un triángulo'
+else
+    r = 'No se puede formar un triángulo!'    
+
 # output
+print("-------------------------------")
+print(r)
+print("-------------------------------")
